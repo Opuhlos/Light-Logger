d538ca9be226af06cd40f9f81109000289268c51
opuhlos-eternity <ericnguyen455@gmail.com>
2025-01-21 22:49:17 -0500

    init Spring Boot initializr package thingy

---
+++ lightlogger/src/main/java/origin/opuhlos/lightlogger/LightloggerApplication.java
@@ -0,0 +1,13 @@
+package origin.opuhlos.lightlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightloggerApplication.class, args);
+	}
+
+}
---
+++ lightlogger/src/test/java/origin/opuhlos/lightlogger/LightloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package origin.opuhlos.lightlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
